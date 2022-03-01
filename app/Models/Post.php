bb6221494f8a9795b01a576f1fa0560c8855d307
estebanpatinog <esteban.patinog@autonoma.edu.co>
2022-03-01 17:26:57 -0500

    Se agregaron las relaciones entre usuario y post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
 class Post extends Model
 {
     use HasFactory;
@@ -12,4 +12,9 @@ class Post extends Model
     protected $fillable = [
         'content',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
