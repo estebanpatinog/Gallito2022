7407acdc3cd75ffd9536281d5329c2c946f824f7
estebanpatinog <esteban.patinog@autonoma.edu.co>
2022-03-08 17:54:46 -0500

    Se agrego el borrar post

--- resources/views/posts/subview-post.blade.php
+++ resources/views/posts/subview-post.blade.php
@@ -1,16 +1,31 @@
 <div class="card mb-2">
     <div class="card-body">
         <div class="">
-            <a href="{{ route('posts.edit', $post->id) }}">
+            <a href="{{ route('posts.edit', $post->id) }}" class = "btn btn-info">
                 <i class="fa-solid fa-pen-to-square" title="Editar Post"></i>
             </a>
-            <a href="">
+
+
+            
+
+            {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete']) !!}
+            {!! Form::button('<i class="fa-solid fa-trash-can"></i>', [
+                'type' => 'submit',
+                'title' => "Remover Post",
+                'class' => 'btn btn-danger',
+                'onclick' => "return confirm('¿Está seguro?')"
+            ]) !!}
+            {{-- <a href="#" onclick="">
                 <i class="fa-solid fa-trash-can" title="Remover Post"></i>
-            </a>
+            </a> --}}
+
+            {!! Form::close() !!}
+
+
 
 
         </div>
-        
+
         <h5 class="card-title">{{ $user->name }}</h5>
         <h6 class="card-subtitle mb-2 text-muted">{{ $post->created_at->diffForHumans()}}</h6>
         <p class="card-text">{{ $post->content }}</p>
